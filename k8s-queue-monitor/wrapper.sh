0f05637ad08246ffa00df529a15a25a0b67d9ad8
Akansha Bansal <akansha.bansal@amd.com>
2025-05-26 21:33:17 -0700

    Update wrapper.sh

--- k8s-queue-monitor/wrapper.sh
+++ k8s-queue-monitor/wrapper.sh
@@ -2,6 +2,10 @@
 # K8s Queue Monitor Wrapper Script
 cd "$(dirname "$0")"
 
+export K8S_QUEUE_MONITOR_KUBECTL_PATH="/usr/local/bin/kubectl"
+export K8S_QUEUE_MONITOR_KUBECONFIG="/home/USERNAME/.kube/config"
+export K8S_QUEUE_MONITOR_OUTPUT_DIR="/home/USERNAME/k8s-queue-monitor-data"
+
 # Create logs directory if it doesn't exist
 mkdir -p logs
 
